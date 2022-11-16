df5ac97b0aaab23da1bbb1eb7ddf64eca7fa7534
bulo4kasmyasom <benetoru@gmail.com>
2022-11-16 21:05:23 +0300

    change method name

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,14 +2,12 @@ import org.junit.Test;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Timeout;
 
-import java.util.concurrent.TimeUnit;
-
 public class MainTest {
 
     @Test
     @Timeout(value = 22)
     @Disabled
-    public void checkTimeExecute() throws Exception {
+    public void checkTimeoutExecuteMainMethod() throws Exception {
         Main.main(null);
     }
 
